217827a4a516a7aca44c23d222cef7a27a8964ff
IevgenR <rea2005@ukr.net>
2021-08-30 16:56:22 +0300

    aad dynamic GET route

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ const mockUserData=[
      
   });
 
+
+  app.get('/users/:id', function(req,res){
+    console.log(req.params.id);
+    res.json({
+       success: true,
+       message: 'successfully got users. Nice!',
+       users: req.params.id
+    });
+    console.log("Send responce to JSON with", req.params.id);
+    
+    
+ });
+
 app.listen(8000, function test(){console.log(
 "Running 8000 port") ;
 });
